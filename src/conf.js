3efba3f71cd62f6da10d5ca01f382ffd5f4523e8
wiyris <github.vvj7hcwsxlc@wiyr.me>
2025-07-08 14:03:00 +0900

    Add scroll settings

--- src/conf.js
+++ src/conf.js
@@ -11,6 +11,8 @@ export default {
     focusFirstCandidate: false,
     richHintsForKeystroke: 1,
     defaultSearchEngine: "bs",
+    smoothScroll: false,
+    scrollStepSize: 142,
     stealFocusOnLoad: false,
     theme,
   },
