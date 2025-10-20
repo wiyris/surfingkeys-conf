11da4a925f2fe03f37cfdefd259f27603c61efb5
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-10-21 08:13:57 +0900

    feat(theme): update theme

--- src/theme.js
+++ src/theme.js
@@ -2,18 +2,13 @@ import api from "./api.js"
 
 const { Hints, Visual } = api
 
-// Hints.style(
-//   "border: solid 2px #6e738d; color:#8aadf4; font-size: 12px; font-weight: bold; background: initial; background-color: #1e2030;",
-// )
-// Hints.style(
-//   "border: solid 2px #6e738d !important; padding: 1px !important; color: #939ab7 !important; background: #181926 !important;",
-//   "text",
-// )
-
-const hintsCss =
-  "font-size: 13pt; font-family: 'JetBrains Mono NL', 'Cascadia Code', 'Helvetica Neue', Helvetica, Arial, sans-serif; border: 0px; color: #cdd6f4 !important; background: #1e1e2e; background-color: #1e1e2e";
-api.Hints.style(hintsCss);
-api.Hints.style(hintsCss, "text");
+Hints.style(
+  "border: solid 2px #6e738d; color:#8aadf4; font-size: 12px; font-weight: bold; background: initial; background-color: #1e2030;",
+)
+Hints.style(
+  "border: solid 2px #6e738d !important; padding: 1px !important; color: #939ab7 !important; background: #181926 !important;",
+  "text",
+)
 
 Visual.style("marks", "background-color: #ed8796;")
 Visual.style("cursor", "background-color: #8aadf4;")
@@ -21,19 +16,18 @@ Visual.style("cursor", "background-color: #8aadf4;")
 export default `
 :root {
   /* Font */
-  // --font: 'JetBrainsMono Nerd Font';
   --font: 'Maple Mono NF';
-  --font-size: 16.0px;
-  // --font-weight: bold;
+  --font-size: 18.0px;
+  --font-weight: bold;
 
-  --fg: #cdd6f4;
-  --bg: #1e1e2e;
-  --bg-dark: #181825;
-  --border: #6c7086;
-  --main-fg: #cdd6f4;
-  --accent-fg: #b4befe;
-  --info-fg: #cba6f7;
-  --select: #45475a;
+  --fg: #cad3f5;
+  --bg: #24273a;
+  --bg-dark: #1e2030;
+  --border: #6e738d;
+  --main-fg: #cad3f5;
+  --accent-fg: #b7bdf8;
+  --info-fg: #ed8796;
+  --select: #494d64;
 }
 
 /* ---------- Generic ---------- */
@@ -44,7 +38,6 @@ color: var(--fg);
   border-color: var(--border);
   font-family: var(--font);
   font-size: var(--font-size);
-  font-weight: var(--font-weight);
 }
 
 input {
@@ -250,156 +243,3 @@ input {
   font-weight: var(--font-weight);
 }
 `
-
-/*
-
-  body {
-    font-family: "Input Mono", "DejaVu Sans Mono", DejaVu, Arial, sans-serif;
-    font-size: 12pt;
-  }
-
-  #sk_keystroke kbd {
-    font-family: "Sudo Nerd Font Mono", "Sudo Mono", "Sudo",
-      "Input Mono Nerd Font", "Input Mono", "DejaVu Sans Mono", "DejaVu", "Arial",
-      sans-serif;
-    font-size: 10pt;
-  }
-
-  #sk_omnibarSearchArea {
-    margin: 0 !important;
-    padding: 0.5rem 1rem !important;
-    border-bottom: none !important;
-  }
-
-  #sk_omnibarSearchResult {
-    margin: 0 !important;
-  }
-
-  #sk_omnibar li {
-    background: none !important;
-    padding: 0.35rem 0.5rem !important;
-  }
-
-  #sk_omnibarSearchResult > ul:nth-child(1) {
-    margin-bottom: 0px !important;
-    padding: 0 !important;
-    padding-bottom: 10px !important;
-  }
-
-  #sk_omnibar .separator {
-    padding-left: 8px !important;
-  }
-
-  @media (prefers-color-scheme: light) {
-    body {
-      color: #483270;
-    }
-
-    #sk_omnibar {
-      background-color: #f5f3fd !important;
-      color: #59446f !important;
-      box-shadow: 0px 3px 15px -6px rgba(53, 13, 81, 0.7) !important;
-    }
-
-    #sk_omnibar .prompt {
-      color: #c2b2d7 !important;
-    }
-
-    #sk_omnibar .separator {
-      color: #d4b1ff !important;
-    }
-
-    #sk_omnibar input {
-      color: #351d53 !important;
-    }
-
-    #sk_omnibarSearchResult {
-      border-top: 1px solid #e1cff5 !important;
-    }
-
-    #sk_omnibar li.focused {
-      background-color: #e1ddff !important;
-      color: #351d53 !important;
-    }
-
-    #sk_banner,
-    #sk_keystroke {
-      border: 1px solid #d7b0ff;
-      background: #e9d9ee;
-    }
-
-    #sk_keystroke .annotation {
-      color: #483270;
-    }
-
-    #sk_keystroke kbd {
-      color: black;
-      background: white;
-    }
-
-    #sk_keystroke kbd .candidates {
-      color: #ff7a75;
-    }
-  }
-
-  @media (prefers-color-scheme: dark) {
-    body {
-      color: #d7b0ff;
-    }
-
-    #sk_omnibar {
-      background-color: #2a323e;
-      color: #cad1d7;
-    }
-
-    #sk_omnibar .prompt {
-      color: #eef5fb !important;
-    }
-
-    #sk_omnibar .separator {
-      color: #8af4ff !important;
-      padding-left: 8px !important;
-    }
-
-    #sk_omnibar input {
-      color: white !important;
-    }
-
-    #sk_omnibarSearchResult {
-      border-top: 1px solid #545f6f !important;
-    }
-
-    #sk_omnibar li.focused {
-      background: #181d24 !important;
-      color: #eef5fb !important;
-    }
-
-    #sk_banner,
-    #sk_keystroke {
-      border: 1px solid #d7b0ff;
-      background: #483270;
-    }
-
-    #sk_keystroke .annotation {
-      color: #d7b0ff;
-    }
-
-    #sk_keystroke kbd {
-      color: #fff;
-      background: #7a57a4;
-      border: 1px solid #2d0080;
-      box-shadow: none;
-    }
-
-    #sk_keystroke kbd .candidates {
-      color: #ff8cf8;
-    }
-  }
-
-  .expandRichHints {
-    animation: none;
-  }
-  .collapseRichHints {
-    animation: none;
-  }
-*/
