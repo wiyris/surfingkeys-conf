2b89d532df6a177dea49d951be0c52b73826d77b
wiyris <c8b5805d-928f-4c0b-8fc0-27817abedf44@passmail.com>
2025-12-29 00:54:03 +0900

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodejs-18_x
+      ];
+    };
+  };
+}
